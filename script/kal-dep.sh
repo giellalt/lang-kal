7edababf673871460600fd8ebb19f09cf3af963c
Trond Trosterud <trond.trosterud@uit.no>
2010-09-03 08:52:58 +0000

    generalised home dir
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/st/kal@33334 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- script/kal-dep.sh
+++ script/kal-dep.sh
@@ -9,9 +9,9 @@ do                                          # run the following loop
 echo -n "Skriv sætning: "                   # (message to user)
 read sentence                               # next 3 lines is the usual command
 echo $sentence | preprocess --abbr=$HOME/gtsvn/st/kal/bin/abbr.txt | \
-lookup -flags mbTT -utf8 ~/st/kal/bin/kal.fst | ~/gt/script/lookup2cg | \
-vislcg3 -g $HOME/st/kal/src/kal-dis3.rle | \
-vislcg3 --grammar $HOME/gt/sme/src/sme-dep.rle -C UTF-8
+lookup -flags mbTT -utf8 $HOME/gtsvn/st/kal/bin/kal.fst | $HOME/gtsvn/gt/script/lookup2cg | \
+vislcg3 -g $HOME/gtsvn/st/kal/src/kal-dis3.rle | \
+vislcg3 --grammar $HOME/gtsvn/gt/sme/src/sme-dep.rle -C UTF-8
 
 
 done                      
