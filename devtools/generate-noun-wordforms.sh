128d5790013370af20a8f296740196352eca36a4
Sjur Moshagen <sjurnm@mac.com>
2017-06-28 23:19:07 +0000

    Adapted the word form generation script to the needs of KAL. First version. Did already uncover some errors.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/kal@154640 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- devtools/generate-noun-wordforms.sh
+++ devtools/generate-noun-wordforms.sh
@@ -17,25 +17,24 @@ fi
 
 ######### USER Variables - change these to your liking: #########
 # Codes for the word forms to be generated - list as many or few as needed:
-morf_codes="+N+Sg+Nom \
-            +N+Sg+Gen \
-            +N+Sg+Ill \
-            +N+Sg+Com \
-            +N+Pl+Nom \
-            +N+Pl+Com \
-            +N+Ess"
+morf_codes="+N+Abs+Sg \
+            +N+Rel+Sg \
+            +N+Trm+Sg \
+            +N+Abl+Sg \
+            +N+Abs+Pl \
+            +N+Lok+Pl"
 
 # Lexicon source file for lexicons and lemmas:
 source_file=src/morphology/stems/nouns.lexc
 
 # Lexicons that should NOT be used to extract lemmas (egrep expression):
-exception_lexicons="(flagK)"
+exception_lexicons="(xxx| K )"
 
 # FST used for generation, MINUS suffix:
 generator_file=src/generator-gt-norm
 
 # How many lemmas maximally for each lexicon:
-lemmacount=10
+lemmacount=25
 
 # Specify path to the dir containing the script used for generation:
 script_dir=$giella_core/scripts
