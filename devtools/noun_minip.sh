4102ec2c024586d22fb06ef778d62b3e0c8a3ae2
Trond Trosterud <trond.trosterud@uit.no>
2022-10-12 12:50:10 +0200

    Add support for hfst. To put it into use: Change # mark from hfst line to xfst line (check in a switch if you use hfst as default).

--- devtools/noun_minip.sh
+++ devtools/noun_minip.sh
@@ -11,6 +11,7 @@
 
 
 LOOKUP=$(echo $LOOKUP)
+HLOOKUP=$(echo $HLOOKUP)
 GTLANGS=$(echo $GTLANGS)
 
 
@@ -24,7 +25,8 @@ for lemma in $(cat $L_FILE);
 do
  for form in $(cat $P_FILE);
  do
-   echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-kal/src/generator-gt-norm.xfst
+   echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-kal/src/generator-gt-norm.xfst    # for xfst
+#  echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-kal/src/generator-gt-norm.hfstol # for hfst
  done
  rm -f $L_FILE
 done
