f8b26bcfebb939a3b84a7ac723b008048685b5a6
Judithe Denbæk <judithe@oqaasileriffik.gl>
2023-12-15 22:40:32 +0000

    filer, der endnu ikke fungerer

---
+++ devtools/trans_minip.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# script to generate paradigms for generating word forms
+# command, when you are in smj:
+# sh devtools/trans_minip.sh HTR_r_GAQ_i | less
+# sh devtools/trans_minip.sh usiler 
+
+
+LOOKUP=$(echo $LOOKUP)
+HLOOKUP=$(echo $HLOOKUP)
+GTLANGS=$(echo $GTLANGS)
+
+
+
+PATTERN=$1
+L_FILE="in.txt"
+cut -d '!' -f1 src/fst/stems/verbs.lexc | egrep $PATTERN | tr '[+ ]' ':'| cut -d ':' -f1>$L_FILE
+
+P_FILE="test/data/testtransparadigm.txt"
+
+for lemma in $(cat $L_FILE);
+do
+ for form in $(cat $P_FILE);
+ do
+   echo "${lemma}${form}" | hfst-lookup -q src/generator-gt-norm.hfstol
+ done
+ rm -f $L_FILE
+done
