c5b6088b3f4a85958d0aedf42cf06eed9915bf76
Sjur N Moshagen <sjurnm@mac.com>
2024-01-24 10:12:36 +0200

    [Template merge] Adjust path after fst build reorg

--- devtools/generate-noun-wordforms.sh
+++ devtools/generate-noun-wordforms.sh
@@ -26,13 +26,13 @@ morf_codes="+N+Abs+Sg \
             +N+Lok+Pl"
 
 # Lexicon source file for lexicons and lemmas:
-source_file=src/fst/stems/nouns.lexc
+source_file=src/fst/morphology/stems/nouns.lexc
 
 # Lexicons that should NOT be used to extract lemmas (egrep expression):
 exception_lexicons="(xxx| K )"
 
 # FST used for generation, MINUS suffix:
-generator_file=src/generator-gt-norm
+generator_file=src/fst/generator-gt-norm
 
 # How many lemmas maximally for each lexicon:
 lemmacount=5
