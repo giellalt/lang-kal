3a2ef5d0b12ed3dced76c13c0811dc92f47085c7
Judithe Denbæk <judithe@oqaasileriffik.gl>
2024-08-28 15:12:00 +0200

    created propernoun paradigm tester

---
+++ devtools/propnoun.minip.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# script to generate paradigms for generating word forms
+# command:
+# sh generate_contlex_para.sh PATTERN
+# example, when you are in kal:
+# sh devtools/noun_minip.sh LAAVU | less
+# sh devtools/noun_minip.sh smiergâs 
+# Only get the lemma you ask for:
+# sh devtools/noun_minip.sh '^smiergâs[:+]' 
+
+
+LOOKUP=$(echo $LOOKUP)
+HLOOKUP=$(echo $HLOOKUP)
+GTLANGS=$(echo $GTLANGS)
+
+
+PATTERN=$1
+L_FILE="in.txt"
+cut -d '!' -f1 ../src/fst/morphology/stems/propernouns.lexc | egrep $PATTERN | tr '[+ ]' ':'| cut -d ':' -f1>$L_FILE
+
+P_FILE="../test/data/testpropnounparadigm.txt"
+
+for lemma in $(cat $L_FILE);
+do
+ for form in $(cat $P_FILE);
+ do
+#   echo "${lemma}${form}" | $LOOKUP $GTLANGS/kal/src/fst/generator-gt-norm.xfst    # for xfst
+  echo "${lemma}${form}" | hfst-lookup -q $GTLANGS/kal/src/fst/generator-gt-norm.hfstol # for hfst
+ done
+ rm -f $L_FILE
+done
