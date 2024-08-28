9b1265f234c42d483bfb485e1cf6c87c82bb305f
Judithe Denbæk <judithe@oqaasileriffik.gl>
2024-08-28 14:37:08 +0200

    noun paradigm tester paths fixed

--- devtools/noun_minip.sh
+++ devtools/noun_minip.sh
@@ -17,16 +17,16 @@ GTLANGS=$(echo $GTLANGS)
 
 PATTERN=$1
 L_FILE="in.txt"
-cut -d '!' -f1 src/fst/stems/nouns.lexc | egrep $PATTERN | tr '[+ ]' ':'| cut -d ':' -f1>$L_FILE
+cut -d '!' -f1 ../src/fst/morphology/stems/nouns.lexc | egrep $PATTERN | tr '[+ ]' ':'| cut -d ':' -f1>$L_FILE
 
-P_FILE="test/data/testnounparadigm.txt"
+P_FILE="../test/data/testnounparadigm.txt"
 
 for lemma in $(cat $L_FILE);
 do
  for form in $(cat $P_FILE);
  do
-#   echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-kal/src/generator-gt-norm.xfst    # for xfst
-  echo "${lemma}${form}" | hfst-lookup -q src/generator-gt-norm.hfstol # for hfst
+#   echo "${lemma}${form}" | $LOOKUP $GTLANGS/kal/src/fst/generator-gt-norm.xfst    # for xfst
+  echo "${lemma}${form}" | hfst-lookup -q $GTLANGS/kal/src/fst/generator-gt-norm.hfstol # for hfst
  done
  rm -f $L_FILE
 done
