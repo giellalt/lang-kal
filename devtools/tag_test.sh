ae96b42ff9b4ff41b314f6a2ce727e52aee98b94
Judithe Denbæk <judithe@oqaasileriffik.gl>
2025-08-29 12:37:47 +0000

    adjusted tag_test.sh to capture flags better

--- devtools/tag_test.sh
+++ devtools/tag_test.sh
@@ -1,9 +1,9 @@
 #shell script to see if there are tags which are not declared in root.lexc or if tags are misspelled
     
 echo 'Are there tags not declared in root.lexc or misspelled?'
-cat  src/fst/morphology/affixes/*lexc src/fst/morphology/stems/*lexc  ../shared-mul/src/fst/stems/*lexc |cut -d '!' -f1 |grep ' ;' | cut -d ':' -f1 |rev |cut -d ' ' -f1 |rev |sed 's/^ //' | cut -d ' ' -f1 |sed 's/+/¢+/g' |sed 's/@/¢@/g'|tr '¢' '\n' | tr '#"' '\n'| egrep '(\+|@)' |sort -u | egrep -v '^(\+|\+%|\+\/\-|\+Cmp\-|\+Cmp%\-|\@0|\@%)$' > lexctags
+cat  src/fst/morphology/affixes/*lexc src/fst/morphology/stems/*lexc  ../shared-mul/src/fst/stems/*lexc |cut -d '!' -f1 |grep ' ;' | cut -d ':' -f1 |rev |cut -d ' ' -f1 |rev |sed 's/^ //' | cut -d ' ' -f1 |sed 's/+/¢+/g' |sed 's/@\(.*\)@/¢@\1@/g'|tr '¢' '\n' | tr '#"' '\n'| egrep '(\+|@)' |sort -u | egrep -v '^(\+|\+%|\+\/\-|\+Cmp\-|\+Cmp%\-|\@0|\@%)$' > lexctags
 
-cat src/fst/morphology/root.lexc |cut -d '!' -f1 |cut -d ':' -f1 |sed 's/+/¢+/g'|sed 's/@/¢@/g' |tr '¢' '\n' | egrep '(\+|@)' |tr -d ' ' | tr -d '\t'|sort -u > roottags
+cat src/fst/morphology/root.lexc |cut -d '!' -f1 |cut -d ':' -f1 |sed 's/+/¢+/g'|sed 's/@\(.*\)@/¢@\1@/g' |tr '¢' '\n' | egrep '(\+|@)' |tr -d ' ' | tr -d '\t'|sort -u > roottags
 
 echo '\nUsed in *.lexc files, but not declared in root.lexc:'
 
