f8b7c36ffdcfb12c2c8f6ec2d826b475690f6faf
Judithe Denbæk <judithe@oqaasileriffik.gl>
2025-09-01 19:06:59 +0000

    removed redundancies

--- devtools/tag_test.sh
+++ devtools/tag_test.sh
@@ -1,9 +1,9 @@
 #shell script to see if there are tags which are not declared in root.lexc or if tags are misspelled
     
 echo 'Are there tags not declared in root.lexc or misspelled?'
-cat  src/fst/morphology/affixes/*lexc src/fst/morphology/stems/*lexc  ../shared-mul/src/fst/stems/*lexc | sed 's/< "\(.*\)" >/\1: ;\n/g' | sed 's/" "/: ;\n/g' |cut -d '!' -f1 |grep ' ;' | cut -d ':' -f1 |rev |cut -d ' ' -f1 |rev |sed 's/^ //' | cut -d ' ' -f1 | sed 's/AA+\([anna|av|ik|inna|innga|kan|kanna|kig|kinnga|ma|manna|panna|pav|pik|pinnga|qam|qanna|qav|sam|sanna|una]\)/\1/g' | sed 's/TA+\([anna|av|ik|inna|innga|kan|kanna|kig|kinnga|ma|manna|panna|pav|pik|pinnga|qam|qanna|qav|sam|sanna|una]\)/\1/g' |sed 's/+/¢+/g'|sed 's/@\(.*\)@/\n@\1@\n/g'| sed 's/@@/@\n@/g' | sed 's/< /\n/g' | sed 's/ >/\n/g' | sed 's/"@\(.*\)@"/@\1@\n/g' | sed 's/@" "@/@\n@/g' | tr '¢' '\n' | tr '#"' '\n' | egrep '(\+|@)' |sort -u | egrep -v '^(\+|\+%|\+\/\-|\+Cmp\-|\+Cmp%\-|\@0|\@%)$' > lexctags
+cat  src/fst/morphology/affixes/*lexc src/fst/morphology/stems/*lexc  ../shared-mul/src/fst/stems/*lexc | sed 's/< "\(.*\)" >/\1: ;\n/g' | sed 's/" "/: ;\n/g' |cut -d '!' -f1 |grep ' ;' | cut -d ':' -f1 |rev |cut -d ' ' -f1 |rev |sed 's/^ //' | cut -d ' ' -f1 | sed 's/AA+\([anna|av|ik|inna|innga|kan|kanna|kig|kinnga|ma|manna|panna|pav|pik|pinnga|qam|qanna|qav|sam|sanna|una]\)/\1/g' | sed 's/TA+\([anna|av|ik|inna|innga|kan|kanna|kig|kinnga|ma|manna|panna|pav|pik|pinnga|qam|qanna|qav|sam|sanna|una]\)/\1/g' |sed 's/+/¢+/g'|sed 's/@\(.*\)@/\n@\1@\n/g'| sed 's/@@/@\n@/g' | tr '¢' '\n' | tr '#"' '\n' | egrep '(\+|@)' |sort -u | egrep -v '^(\+|\+%|\+\/\-|\+Cmp\-|\+Cmp%\-|\@0|\@%)$' > lexctags
 
-cat src/fst/morphology/root.lexc |cut -d '!' -f1 |cut -d ':' -f1 |sed 's/+/¢+/g'|sed 's/@\(.*\)@/¢@\1@/g' |tr '¢' '\n' | egrep '(\+|@)' |tr -d ' ' | tr -d '\t'|sort -u > roottags
+cat src/fst/morphology/root.lexc | sed 's/AA+//g' | sed 's/TA+//g' |cut -d '!' -f1 |cut -d ':' -f1 |sed 's/+/¢+/g'|sed 's/@\(.*\)@/¢@\1@/g' |tr '¢' '\n' | egrep '(\+|@)' |tr -d ' ' | tr -d '\t'|sort -u > roottags
 
 echo '\nUsed in *.lexc files, but not declared in root.lexc:'
 
