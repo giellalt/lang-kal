1ee1bf45f059bec31f8603d5c521509935309938
Judithe Denbæk <judithe@oqaasileriffik.gl>
2025-09-01 17:06:03 +0000

    more accurate flag tester w/o false results + ignore prefixes in test

--- devtools/tag_test.sh
+++ devtools/tag_test.sh
@@ -1,7 +1,7 @@
 #shell script to see if there are tags which are not declared in root.lexc or if tags are misspelled
     
 echo 'Are there tags not declared in root.lexc or misspelled?'
-cat  src/fst/morphology/affixes/*lexc src/fst/morphology/stems/*lexc  ../shared-mul/src/fst/stems/*lexc |cut -d '!' -f1 |grep ' ;' | cut -d ':' -f1 |rev |cut -d ' ' -f1 |rev |sed 's/^ //' | cut -d ' ' -f1 |sed 's/+/¢+/g' |sed 's/@\(.*\)@/¢@\1@/g'|tr '¢' '\n' | tr '#"' '\n'| egrep '(\+|@)' |sort -u | egrep -v '^(\+|\+%|\+\/\-|\+Cmp\-|\+Cmp%\-|\@0|\@%)$' > lexctags
+cat  src/fst/morphology/affixes/*lexc src/fst/morphology/stems/*lexc  ../shared-mul/src/fst/stems/*lexc | sed 's/< "\(.*\)" >/\1: ;\n/g' | sed 's/" "/: ;\n/g' |cut -d '!' -f1 |grep ' ;' | cut -d ':' -f1 |rev |cut -d ' ' -f1 |rev |sed 's/^ //' | cut -d ' ' -f1 | sed 's/AA+\([anna|av|ik|inna|innga|kan|kanna|kig|kinnga|ma|manna|panna|pav|pik|pinnga|qam|qanna|qav|sam|sanna|una]\)/\1/g' | sed 's/TA+\([anna|av|ik|inna|innga|kan|kanna|kig|kinnga|ma|manna|panna|pav|pik|pinnga|qam|qanna|qav|sam|sanna|una]\)/\1/g' |sed 's/+/¢+/g'|sed 's/@\(.*\)@/\n@\1@\n/g'| sed 's/@@/@\n@/g' | sed 's/< /\n/g' | sed 's/ >/\n/g' | sed 's/"@\(.*\)@"/@\1@\n/g' | sed 's/@" "@/@\n@/g' | tr '¢' '\n' | tr '#"' '\n' | egrep '(\+|@)' |sort -u | egrep -v '^(\+|\+%|\+\/\-|\+Cmp\-|\+Cmp%\-|\@0|\@%)$' > lexctags
 
 cat src/fst/morphology/root.lexc |cut -d '!' -f1 |cut -d ':' -f1 |sed 's/+/¢+/g'|sed 's/@\(.*\)@/¢@\1@/g' |tr '¢' '\n' | egrep '(\+|@)' |tr -d ' ' | tr -d '\t'|sort -u > roottags
 
@@ -22,7 +22,7 @@ echo '\nChecking for mismatches in Der-tags .n-v.'
 git ls-files -- "*.lexc" | xargs -n1 git blame -f | grep -P '\+[A-Z]+\+Der/.n\+[A-Z]+\+Der/v.'
 
 echo '\nChecking for plus sign duplicates'
-git ls-files -- "*.lexc" | xargs -n1 git blame -f | grep -P '\+\+([A-Z]+|Der)'
+git ls-files -- "*.lexc" | xargs -n1 git blame -f | grep -P '\+\+[A-Z]'
 
 echo '\nChecking for use of equals sign instead of plus sign'
 git ls-files -- "*.lexc" | xargs -n1 git blame -f | grep -P '[a-z]+=[A-Z]+\+Der'
