4daf6bd114ee0027b0e527a5d394cafe41d09e22
Judithe Denbæk <judithe@oqaasileriffik.gl>
2026-07-05 21:11:33 +0200

    aarluk i miniparadigme

--- devtools/strong_nouns_minip.sh
+++ devtools/strong_nouns_minip.sh
@@ -6,23 +6,31 @@
 HLOOKUP="hfst-lookup -q"
 GTLANGS=$(echo $GTLANGS)
 
-PATTERNTUP="erneq"
-PATTERNTUPTR="atileq"
+PATTERNZ2ZMORF="erneq"
+PATTERNZ2_QZMORF="atileq"
+PATTERNZ2_ZMORF="aarluk"
 
-TUP_FILE="src/fst/morphology/test/test-tup-inflections.txt"
-TUPTR_FILE="src/fst/morphology/test/test-tup-tr-inflections.txt"
+TUP_FILE="src/fst/morphology/test/test-tup-inflections.txt"				# tup_inflections
+TUPTR_FILE="src/fst/morphology/test/test-tup-tr-inflections.txt"		# tup_tr_inflections
 
-for lemma1 in $(echo $PATTERNTUP);
+for lemma1 in $(echo $PATTERNZ2ZMORF);
 do
  for form1 in $(cat $TUP_FILE);
  do
   echo "${lemma1}${form1}" | $HLOOKUP $GTLANGS/kal/src/fst/generator-gt-desc.hfstol # desc for at teste alt er med
 done
-for lemma2 in $(echo $PATTERNTUPTR);
+for lemma2 in $(echo $PATTERNZ2_QZMORF);
 do
  for form2 in $(cat $TUPTR_FILE);
  do
   echo "${lemma2}${form2}" | $HLOOKUP $GTLANGS/kal/src/fst/generator-gt-desc.hfstol
  done
+for lemma3 in $(echo $PATTERNZ2_ZMORF);
+do
+ for form2 in $(cat $TUPTR_FILE);
+ do
+  echo "${lemma3}${form2}" | $HLOOKUP $GTLANGS/kal/src/fst/generator-gt-desc.hfstol
+ done
+done
 done
 done
